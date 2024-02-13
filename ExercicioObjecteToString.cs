e90c5a33315df70ff2e223fdba3fe3d392825669
Victor Vinicius Alves de L. Souza <96602671+Foqsz@users.noreply.github.com>
2024-02-13 09:07:17 -0300

    Update ExercicioObjecteToString.cs

--- ExercicioObjecteToString.cs
+++ ExercicioObjecteToString.cs
@@ -25,3 +25,31 @@ namespace Course
         }
     }
 }
+
+using System;
+using System.Globalization;
+
+namespace Course
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+        public override string ToString()
+        {
+            return Nome
+               + ", $ "
+               + Preco.ToString("F2", CultureInfo.InvariantCulture)
+               + ", "
+               + Quantidade
+               + " unidades, Total: $ "
+               + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+
+        }
+    }
+}
