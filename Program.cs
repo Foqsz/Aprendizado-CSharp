1759fc03cf7a9f2fc260736a987bfa0d3c8d53cc
Victor Vinicius Alves de L. Souza <96602671+Foqsz@users.noreply.github.com>
2024-02-13 08:24:33 -0300

    Studies in C#
    using object orientation for the first time

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario A = new Funcionario();
+            Funcionario B = new Funcionario();
+
+            Console.WriteLine("Dados da Primeira Pessoa:");
+            Console.Write("Nome: ");
+            A.Nome = Console.ReadLine();
+
+            Console.Write("Salario: "); 
+            A.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Dados da Segunda Pessoa:");
+            Console.Write("Nome: ");
+            B.Nome = Console.ReadLine();
+
+            Console.Write("Salario: ");
+            B.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double media;
+
+            media = (A.Salario + B.Salario) / 2;
+            Console.WriteLine("Salario Medio: " + media);
+
+        }
+    }
+}
