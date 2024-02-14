52ff0e74c4c70c9568c93346bb7bb4e029c450fe
Victor Vinicius Alves de L. Souza <96602671+Foqsz@users.noreply.github.com>
2024-02-14 09:31:21 -0300

    Update c# - Using static members without objects.cs

--- c# - Using static members without objects.cs
+++ c# - Using static members without objects.cs
@@ -17,3 +17,25 @@ namespace Course
         }
     }
 }
+
+using System;
+using System.Globalization;
+
+namespace Course // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    { 
+        static void Main(string[] args)
+        {
+
+Calculadora calc = new Calculadora();
+Console.Write("Entre o valor do raio: ");
+double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+double circ = calc.Circunferencia(raio);
+double volume = calc.Volume(raio);
+Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+Console.WriteLine("Valor de PI: " + calc.Pi.ToString("F2", CultureInfo.InvariantCulture));
+
+}   }
+}    
