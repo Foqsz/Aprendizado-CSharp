5852300c5a5e33926e1726a69b45600ddbb4b216
Victor Vinicius Alves de L. Souza <96602671+Foqsz@users.noreply.github.com>
2024-02-14 11:08:20 -0300

    aula 55

--- c# - Encapsulation Learning.cs
+++ c# - Encapsulation Learning.cs
@@ -70,27 +70,6 @@ namespace Course
     }
 }
 
-using Course;
-using System;
-using System.Globalization;
-
-namespace Product
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        { 
-
-            Produto p = new Produto("TV", 500.00, 10);
-
-            p.SetNome("T");
-
-            Console.WriteLine(p.GetNome());
-            Console.WriteLine(p.GetPreco());    
-        }
-    }
-}
-
 ////Modificações de encapsulamento abaixo
 using Course;
 using System;
