e33920cd4e4f516c04f452ca3545fc1fdd68514b
Victor Vinicius Alves de L. Souza <96602671+Foqsz@users.noreply.github.com>
2024-02-21 09:31:45 -0300

    object TimeSpan.cs

--- c# - TimeSpan.cs
+++ c# - TimeSpan.cs
@@ -18,19 +18,19 @@ namespace Course
             Console.WriteLine(t4);
             Console.WriteLine(t5);
 
-          TimeSpan s1 = TimeSpan.FromDays(1.5);
-          TimeSpan s2 = TimeSpan.FromHours(1.5);
-          TimeSpan s3 = TimeSpan.FromMinutes(1.5);
-          TimeSpan s4 = TimeSpan.FromSeconds(1.5);
-          TimeSpan s5 = TimeSpan.FromMilliseconds(1.5);
-          TimeSpan s6 = TimeSpan.FromTicks(900000000L);
-          
-          Console.WriteLine(s1);
-          Console.WriteLine(s2);
-          Console.WriteLine(s3);
-          Console.WriteLine(s4);
-          Console.WriteLine(s5);
-          Console.WriteLine(s6);
+            TimeSpan s1 = TimeSpan.FromDays(1.5);
+            TimeSpan s2 = TimeSpan.FromHours(1.5);
+            TimeSpan s3 = TimeSpan.FromMinutes(1.5);
+            TimeSpan s4 = TimeSpan.FromSeconds(1.5);
+            TimeSpan s5 = TimeSpan.FromMilliseconds(1.5);
+            TimeSpan s6 = TimeSpan.FromTicks(900000000L);
+              
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
+            Console.WriteLine(s4);
+            Console.WriteLine(s5);
+            Console.WriteLine(s6);
 
         }
     }
