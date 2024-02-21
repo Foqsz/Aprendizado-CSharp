d488e30f755136e548dd7e95a6407ce9ba627d92
Victor Vinicius Alves de L. Souza <96602671+Foqsz@users.noreply.github.com>
2024-02-21 09:31:07 -0300

    Object TimeSpan

---
+++ c# - TimeSpan.cs
@@ -0,0 +1,37 @@
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan t1 = new TimeSpan();
+            TimeSpan t2 = new TimeSpan(900000000L);
+            TimeSpan t3 = new TimeSpan(2, 11, 21);
+            TimeSpan t4 = new TimeSpan(1, 2, 11, 21);
+            TimeSpan t5 = new TimeSpan(1, 2, 11, 21, 321);
+
+            Console.WriteLine(t1); 
+            Console.WriteLine(t2);
+            Console.WriteLine(t3);
+            Console.WriteLine(t4);
+            Console.WriteLine(t5);
+
+          TimeSpan s1 = TimeSpan.FromDays(1.5);
+          TimeSpan s2 = TimeSpan.FromHours(1.5);
+          TimeSpan s3 = TimeSpan.FromMinutes(1.5);
+          TimeSpan s4 = TimeSpan.FromSeconds(1.5);
+          TimeSpan s5 = TimeSpan.FromMilliseconds(1.5);
+          TimeSpan s6 = TimeSpan.FromTicks(900000000L);
+          
+          Console.WriteLine(s1);
+          Console.WriteLine(s2);
+          Console.WriteLine(s3);
+          Console.WriteLine(s4);
+          Console.WriteLine(s5);
+          Console.WriteLine(s6);
+
+        }
+    }
+}
