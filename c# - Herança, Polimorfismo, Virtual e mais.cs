746540b0076760f9e5dede3d486fc44b03d99fdd
Victor Vinicius Alves de L. Souza <96602671+Foqsz@users.noreply.github.com>
2024-03-01 11:54:45 -0300

    Create c# - Herança, Polimorfismo, Virtual e mais.cs

---
+++ c# - Herança, Polimorfismo, Virtual e mais.cs
@@ -0,0 +1,63 @@
+//a continuação com as classes deste exercicio estão no meu outro repositorio, c-sharp-basic.
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Course.Entities;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<ProductImport> list = new List<ProductImport>();
+
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Product #{i} data:");
+                Console.Write("Common, use or imported (c/u/i)? ");
+                char ch = char.Parse(Console.ReadLine()); // usaremos a variavel char para responder a pergunta acima, y ou n.
+
+                if (ch == 'i') // leitura: se a variavel ch for igual a letra y em 'ch', ele vai ler.
+                {
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Price: ");
+                    double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    Console.Write("Customs fee: ");
+                    double customsFee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    list.Add(new ImportedProduct(name, price, customsFee));
+                }
+                else if (ch == 'c')
+                {
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Price: ");
+                    double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new ProductImport(name, price));
+                }
+                else
+                {
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Price: ");
+                    double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    Console.Write("Manufacture date (DD/MM/YYYY): ");
+                    DateTime manufactureDate = DateTime.Parse(Console.ReadLine());
+                    list.Add(new UsedProduct(name, price, manufactureDate));
+                }
+            }
+
+            Console.WriteLine("PRICE TAGS: ");
+            foreach (ProductImport prod in list)
+            {
+                Console.WriteLine(prod.PriceTag());
+            } 
+        }
+    }
+}
+
