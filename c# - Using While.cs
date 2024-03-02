bb5fe1a917b6cdc3aee73579f5504e9a7bb7f736
Victor Vinicius Alves de L. Souza <96602671+Foqsz@users.noreply.github.com>
2024-03-02 18:54:16 -0300

    método while basic

---
+++ c# - Using While.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int contador = 0;
+
+        while (contador < 5)
+        {
+            Console.WriteLine("Contador: " + contador);
+            contador++;
+        }
+
+        Console.WriteLine("Loop concluído!");
+    }
+}
